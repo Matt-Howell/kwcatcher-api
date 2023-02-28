3d77aee391c11aa95359f51679c2c7ad9a5c7009
Matt-Howell <73589713+Matt-Howell@users.noreply.github.com>
2023-02-28 21:34:10 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fetch = require("node-fetch")
 
 app.get('/get-kws', async (req, res) => { 
     res.set('Access-Control-Allow-Origin', 'https://oyster-app-3iwrj.ondigitalocean.app')
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    // res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
     async function postData(val) {
         const response = await fetch('https://api.serpsbot.com/v2/google/search-suggestions', {
             method: 'POST',
@@ -66,7 +66,7 @@ app.get('/get-kws', async (req, res) => {
 
 app.get('/analyse-kw', async (req, res) => { 
     res.set('Access-Control-Allow-Origin', 'https://oyster-app-3iwrj.ondigitalocean.app')
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    // res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
     async function postData(val) {
         const response = await fetch('https://api.dataforseo.com/v3/dataforseo_labs/google/historical_search_volume/live', {
             method: 'POST',

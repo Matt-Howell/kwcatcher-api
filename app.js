c0e2aaa0c7fd4997fab4a6a68547148a25fc1c88
Matt-Howell <73589713+Matt-Howell@users.noreply.github.com>
2023-03-23 18:01:54 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config()
 
 app.get('/get-kws', async (req, res) => { 
-    // res.set('Access-Control-Allow-Origin', 'https://beta.keywordcatcher.com')
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.set('Access-Control-Allow-Origin', 'https://beta.keywordcatcher.com')
+    // res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
     async function postData(val) {
         const response = await fetch('https://api.serpsbot.com/v2/google/search-suggestions', {
             method: 'POST',
@@ -68,8 +68,8 @@ app.get('/get-kws', async (req, res) => {
 })
 
 app.get('/analyse-kw', async (req, res) => { 
-    // res.set('Access-Control-Allow-Origin', 'https://beta.keywordcatcher.com')
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.set('Access-Control-Allow-Origin', 'https://beta.keywordcatcher.com')
+    // res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
     async function postData(val) {
         const response = await fetch('https://api.dataforseo.com/v3/dataforseo_labs/google/historical_search_volume/live', {
             method: 'POST',

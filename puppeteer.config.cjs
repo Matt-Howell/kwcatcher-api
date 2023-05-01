0a0de4841cdd68209825fa985881ca2edeadfb46
Matt-Howell <73589713+Matt-Howell@users.noreply.github.com>
2023-05-01 18:56:23 +0100

    Update puppeteer.config.cjs

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -1,6 +1,5 @@
 const {join} = require('path');
 
 module.exports = {
-  // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, 'workspace/.cache', 'puppeteer'),
+  cacheDirectory: join('.cache', 'puppeteer'),
 };
